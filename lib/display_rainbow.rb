566f663205bc049e3b72834065a194cd2ca76645
Ramona M <r1mari@yahoo.com>
2018-06-12 12:30:59 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,4 +4,5 @@ def print_business_card(person)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 end
-person = ["Bob", 46, "Juggler", "555-555-555"]
+bob = ["Bob", 46, "Juggler", "555-555-555"]
+print_business_card(bob)
