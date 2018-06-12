38b38f6e845285a45390ac850b7a950ecefcfdd0
Ramona M <r1mari@yahoo.com>
2018-06-12 12:30:29 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,6 @@
 person = [ ]
 def print_business_card(person)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts "Contact: #{person[3]}"
 end
 person = ["Bob", 46, "Juggler", "555-555-555"]
